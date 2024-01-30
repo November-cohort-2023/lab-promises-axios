7f2f8390082a64de89331ed67dadb81763dc5c4c
Eduardo Botelho <eduardo11224b@gmail.com>
2024-01-30 20:15:10 +0000

    feat: Fetch data from darth vader and log it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className='App'>
-    </div>
-  )
+  async function fetchDarthVaderData() {
+    await axios
+      .get("https://swapi.dev/api/people/4")
+      .then((result) => {
+        console.log(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  fetchDarthVaderData();
+
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
