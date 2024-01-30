792dbfcdc7dce799d6d45c7726d2a82485a2a3d6
Eduardo Botelho <eduardo11224b@gmail.com>
2024-01-30 20:18:14 +0000

    feat: Fetch france name and log it

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,19 @@ function App() {
         console.log(error);
       });
   }
+  async function fetchFranceData() {
+    await axios
+      .get("https://restcountries.com/v3.1/name/france")
+      .then((result) => {
+        console.log(result.data[0].name.common);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   fetchDarthVaderData();
+  fetchFranceData();
 
   return <div className="App"></div>;
 }
