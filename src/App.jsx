bc471943cb5e3ba942577181b7fe678dbc9c2726
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-01-31 12:31:41 +0200

    Iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   axios.get('https://swapi.dev/api/people/4')
   .then((res)=> {
     console.log(res.data)
+    return axios.get('https://restcountries.com/v3.1/name/france')
+  })
+  .then((res)=> {
+    console.log(res, 'res from france')
+    console.log(res.data[0].name.common, 'is the name of france')
+
   })
 
   
