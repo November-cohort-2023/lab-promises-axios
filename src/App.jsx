f1d7eed8d4042dbb566f1efbda6028f1650feda0
Irene <amoreperdesi@gmail.com>
2024-02-01 16:41:42 +0100

    Done all 4 iterations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import './App.css'
+import axios from 'axios'
+
 
 function App() {
+
+  axios.get('https://swapi.dev/api/people/4')
+  .then((darthVaders)=>{
+    console.log(darthVaders.data);
+    return axios.get('https://restcountries.com/v3.1/name/france')
+  })
+  .then((france)=>{
+    console.log(france.data)
+    return axios.get('https://restcountries.com/v3.1/all')
+  })
+  .then((allCountries)=>{
+    console.log(allCountries.data.slice(0,10));
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+
   return (
-    <div className='App'>
-    </div>
+    <>
+    </>
   )
 }
 
